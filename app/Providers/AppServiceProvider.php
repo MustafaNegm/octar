5c9ef9656cdfa670e5aea73ab3ac7b68122d6b1c
ahmedmohamed1101140 <ahmedmohamed1101140@outlook.com>
2018-08-26 14:44:41 +0200

    remove ssl

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,7 +17,7 @@ class AppServiceProvider extends ServiceProvider
         //
         Schema::defaultStringLength(191);
         if ($this->app->environment() == 'prod') {
-            URL::forceScheme('https');
+            URL::forceScheme('http');
         }
     }
 
